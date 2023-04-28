53b77e141395421b48bb21254e3038b95fc871d0
Jahongir Rakhmanov <jahongir.vasitov@gmail.com>
2023-04-28 16:57:49 +0500

    Store Book

--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using my_books.Data;
+using my_books.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddTransient<BooksService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
