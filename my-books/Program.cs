f39f2579dbe4c2ef24c6dd40ad4b01bab77ebf18
Jahongir Rakhmanov <jahongir.vasitov@gmail.com>
2023-04-28 15:18:09 +0500

    installed Microsoft.EntityFrameworkCore.Tools, connected to real Db, migrate

--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using my_books.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
