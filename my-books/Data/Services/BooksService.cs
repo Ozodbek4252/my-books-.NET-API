978bf479b765fd6581cd878c25093a884728946c
Jahongir Rakhmanov <jahongir.vasitov@gmail.com>
2023-04-28 17:37:48 +0500

    Update Book

--- my-books/Data/Services/BooksService.cs
+++ my-books/Data/Services/BooksService.cs
@@ -1,5 +1,7 @@
 ﻿using my_books.Data.Models;
 using my_books.Data.ViewModels;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Threading;
 
 namespace my_books.Data.Services
 {
@@ -34,9 +36,27 @@ namespace my_books.Data.Services
             return _context.Books.ToList();
         }
 
-        public Book GetBookById(int bookId)
+        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(n => n.Id == bookId);
+
+        public Book UpdateBookById(int bookId, BookVM book)
         {
-            return _context.Books.FirstOrDefault(n => n.Id == bookId);
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
+            if (_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.IsRead ? book.DateRead.Value : null;
+                _book.Rate = book.IsRead ? book.Rate.Value : null;
+                _book.Genre = book.Genre;
+                _book.Author = book.Author;
+                _book.CoverUrl = book.CoverUrl;
+                _book.DateAdded = DateTime.Now;
+
+                _context.SaveChanges();
+            }
+
+            return _book;
         }
     }
 }
