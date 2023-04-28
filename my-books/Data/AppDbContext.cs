6902ae60645659a2031baddb5c09ead27a2dab4f
Jahongir Rakhmanov <jahongir.vasitov@gmail.com>
2023-04-28 10:12:40 +0500

    AppDbContext added, fake Db configured, two package installed

---
+++ my-books/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // this is a bridge wetween Models and database
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        // Models
+        public DbSet<Book> Books { get; set; }
+    }
+}
