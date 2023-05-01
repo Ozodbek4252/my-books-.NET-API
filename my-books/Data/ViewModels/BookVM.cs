3bf8ed9739a67d36aa9b302e7f11ce87b2611732
Jahongir Rakhmanov <jahongir.vasitov@gmail.com>
2023-05-01 12:20:40 +0500

    get Authors of a Book

--- my-books/Data/ViewModels/BookVM.cs
+++ my-books/Data/ViewModels/BookVM.cs
@@ -13,4 +13,17 @@
         public int? PublisherId { get; internal set; }
         public List<int> AuthorIds { get; set; }
     }
+    public class BookWithAuthorsVM
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genre { get; set; }
+        public string CoverUrl { get; set; }
+
+        public string PublisherName { get; internal set; }
+        public List<string> AuthorNames { get; set; }
+    }
 }
